95f04f807abf040458d332819a83ac40dc469500
stickw <gentus72@gmail.com>
2022-02-02 10:02:19 +0100

    some polish (round corners) and stuff

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1280,
-    height: 850,
+    height: 720,
   });
 
   // and load the index.html of the app.
