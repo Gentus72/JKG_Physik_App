e84d7eebf5a3c386728873eb6da4361094e39974
stickw <60480277+Gentus72@users.noreply.github.com>
2022-06-28 19:38:15 +0200

    quick save

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+var classFilter = 0, topicFilter = '';
+
 function openNavL() {
   document.getElementById('sidenav-left').style.width = '25vw';
 }
@@ -5,7 +7,7 @@ function openNavL() {
 function closeNavL(choice, filterIndex) {
   document.getElementById('sidenav-left').style.width = '0';
   document.getElementById('filter-class-text').textContent = choice;
-  sortExperiments(filterIndex, _topicFilter);
+  sortExperiments(classFilter, topicFilter);
 }
 
 function openNavR() {
@@ -15,5 +17,5 @@ function openNavR() {
 function closeNavR(choice, filterIndex) {
   document.getElementById('sidenav-right').style.width = '0';
   document.getElementById('filter-topic-text').textContent = choice;
-  sortExperiments(_classFilter, filterIndex);
+  sortExperiments(classFilter, topicFilter);
 }
--- src/sort_experiments.js
+++ src/sort_experiments.js
@@ -6,19 +6,6 @@
    });
 });*/
 
-var _classFilter = 0, _topicFilter = '';
-
-//grab from server / file from server
-var total_experiments = [
-  new Experiment('das Ei in der Flasche', [6, 8, 9], ['mechanik', 'mech_der_fl']),
-  new Experiment('das schwebende Röhrchen', [7, 8], ['mech_der_fl']),
-  new Experiment('Demonstration des Auflagedrucks', [10], ['mechanik', 'energie_in_n_und_t']),
-  new Experiment('platzender Luftballon', [6, 8, 10], ['thermodynamik', 'mech_der_fl']),
-  new Experiment('Wasser im Glas', [5, 7], 'mech_der_fl')
-]
-
-
-
 class Experiment {
   constructor(name, klasse, topic) {
     this.name = name;
@@ -38,5 +25,56 @@ class Experiment {
         return this.topic;
         break;
     }
+  }
+}
+
+//grab from server / file from server
+var total_experiments = [
+  new Experiment('das Ei in der Flasche', [6, 8, 9], ['mechanik', 'mech_der_fl']),
+  new Experiment('das schwebende Röhrchen', [7, 8], ['mech_der_fl']),
+  new Experiment('Demonstration des Auflagedrucks', [10], ['mechanik', 'energie_in_n_und_t']),
+  new Experiment('platzender Luftballon', [6, 8, 10], ['thermodynamik', 'mech_der_fl']),
+  new Experiment('Wasser im Glas', [5, 7], ['mech_der_fl'])
+]
+
+function sortExperiments(classFilter, topicFilter) {
+  //go through every entry of total_experiments and
+  //scan for matches using Experiment.getInfo(filter)
+
+  var result = [];
+
+  console.log(classFilter + ' + ' + topicFilter);
+
+  //not working when class and topic are specified (obviously)
+  for (var e of total_experiments) {
+    if (classFilter != 0) {
+      var _klasse = e.getInfo('klasse');
+
+      for (var i = 0; i < _klasse.length; i++) {
+        if (_klasse[i] == classFilter) {
+          result.push(e);
+        }
+      }
+    }
+
+    if (topicFilter != '') {
+      var _topic = e.getInfo('topic');
+
+      for (var i = 0; i < _topic.length; i++) {
+        if (_topic[i] == topicFilter) {
+          result.push(e);
+        }
+      }
+    }
+  }
+
+  console.log(result);
+  return result;
+}
+
+//TODO implement
+function generateExperimentHTML(sorted_experiments, resultFountToF) {
+  if (resultFountToF) {
+
   }
 }
